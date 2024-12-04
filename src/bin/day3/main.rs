c3a72e1e52dfbb7d598d7cbe833bb923e0ea0b04
george velimachitis <3185416+gvelim@users.noreply.github.com>
2024-12-04 18:08:30 +0000

    refactor into - CPU (stateful/stateless) - Program - Instructions

--- src/bin/day3/main.rs
+++ src/bin/day3/main.rs
@@ -1,43 +1,53 @@
 use std::str::FromStr;
+use std::time::Instant;
 use nom::{bytes::complete::tag, character::complete::{anychar, char}, IResult, sequence::{delimited, separated_pair}, multi::many_till, combinator::value, Parser};
 use nom::branch::alt;
 
 fn main() {
-    let i = std::fs::read_to_string("src/bin/day3/input.txt").unwrap();
-    let pgm = i.parse::<Program>().unwrap();
+    let input = std::fs::read_to_string("src/bin/day3/input.txt").unwrap();
+    let pgm = input.parse::<Program>().map_err(|e| panic!("{e:?}")).unwrap();
+    let mut cpu = CPU::default();
 
+    let t = Instant::now();
     let sum = pgm.instructions
         .iter()
-        .map(|i|
-            match i {
-                &Instruction::Mul(x, y) => x*y,
-                _ => 0
-            }
-        )
+        .filter_map(|&i| cpu.run_instruction(i))
         .sum::<u32>();
-    println!("part1: {}",sum);
+    println!("part1: {} - {:?}",sum, t.elapsed());
     assert_eq!(185797128,sum);
 
-    let mut run = true;
+    let t = Instant::now();
+    cpu.set_enhanced_instructions(true);
     let sum = pgm.instructions
         .iter()
-        .filter(|i| {
-            match i {
-                Instruction::DONT => run = false,
-                Instruction::DO => run = true,
-                _ => ()
-            }
-            run
-        })
-        .map(|i|
-            match i {
-                &Instruction::Mul(x, y) => x*y,
-                _ => 0
-            }
-        )
+        .filter_map(|&i| cpu.run_instruction(i))
         .sum::<u32>();
-    println!("part1: {}",sum);
+    println!("part1: {} - {:?}",sum, t.elapsed());
+    assert_eq!(89798695,sum)
+}
 
+#[derive(Debug)]
+struct CPU {
+    run_state: bool,
+    enhanced: bool
+}
+
+impl CPU {
+    fn run_instruction(&mut self, instruction: Instruction) -> Option<u32> {
+        match instruction {
+            Instruction::Mul(x, y) if self.run_state => Some(x*y),
+            Instruction::DONT if self.enhanced => {self.run_state = false; None}
+            Instruction::DO if self.enhanced => {self.run_state = true; None}
+            _ => None
+        }
+    }
+    fn set_enhanced_instructions(&mut self, state:bool) { self.enhanced = state }
+}
+
+impl Default for CPU {
+    fn default() -> Self {
+        CPU { run_state: true, enhanced: false }
+    }
 }
 
 #[derive(Debug, Copy, Clone)]
@@ -47,7 +57,6 @@ pub enum Instruction {
     DO
 }
 
-#[derive(Debug)]
 struct Program {
     instructions: Vec<Instruction>
 }
@@ -57,16 +66,7 @@ impl FromStr for Program {
     fn from_str(mut s: &str) -> Result<Self, Self::Err> {
         let mut instructions = vec![];
 
-        while let Ok((remaining, (_,instruction)))
-            = many_till(
-                anychar,
-                alt((
-                    parse_instruction,
-                    parse_do,
-                    parse_dont
-                ))
-            )(s)
-        {
+        while let Ok((remaining, (_,instruction))) = many_till(anychar, alt((parse_instruction, parse_do, parse_dont)))(s) {
             instructions.push(instruction);
             s = remaining;
         }
