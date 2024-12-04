a328ca0e92caa9251baba8accae7731adc4c0788
george velimachitis <3185416+gvelim@users.noreply.github.com>
2024-12-04 17:12:37 +0000

    part 1 pass

--- src/bin/day3/main.rs
+++ src/bin/day3/main.rs
@@ -1,31 +1,54 @@
-use nom::bytes::complete::{is_a, tag, take_until, take_while};
-use nom::character::complete::{anychar, char, space0};
-use nom::{IResult};
-use nom::branch::alt;
-use nom::bytes::streaming::take_till;
-use nom::sequence::{delimited, preceded, separated_pair, terminated};
-use nom::error::Error;
-use nom::multi::{fold_many0, many0, many_till};
+use std::str::FromStr;
+use nom::{
+    bytes::complete::tag,
+    character::complete::{anychar, char},
+    IResult,
+    sequence::{delimited, separated_pair},
+    multi::many_till
+};
+
+fn main() {
+    let i = std::fs::read_to_string("src/bin/day3/input.txt").unwrap();
+    let pgm = i.parse::<Program>().unwrap();
+
+    println!("part1: {}",
+        pgm.instructions
+            .iter()
+            .map(|i| match i { &Instruction::Mul(x, y) => x*y })
+            .sum::<u32>()
+    );
+
+}
+
+enum Instruction {
+    Mul(u32,u32)
+}
+
+struct Program {
+    instructions: Vec<Instruction>
+}
+impl FromStr for Program {
+    type Err = ();
+    fn from_str(mut s: &str) -> Result<Self, Self::Err> {
+        let mut instructions = vec![];
+
+        while let Ok((remaining, (_,(x,y)))) = many_till(anychar,parse_mul)(s) {
+            instructions.push(Instruction::Mul(x,y));
+            s = remaining;
+        }
+
+        Ok(Self { instructions })
+    }
+}
 
 fn parse_mul(i: &str) -> IResult<&str, (u32, u32)> {
     delimited(
         tag("mul("),
         separated_pair(
-            delimited(space0,nom::character::complete::u32,space0),
-            delimited(space0,char(','),space0),
-            delimited(space0,nom::character::complete::u32,space0)
+            nom::character::complete::u32,
+            char(','),
+            nom::character::complete::u32,
         ),
         tag(")"),
     )(i)
-}
-
-fn main() {
-    let i = std::fs::read_to_string("src/bin/day3/sample.txt").unwrap();
-    let mut input = i.as_str();
-
-    while let Ok((remaining, result)) = many_till(anychar,parse_mul)(input) {
-        println!("input: {:?}", result.1);
-        input = remaining;
-    }
-
 }
