e0c5fbcff229f4c86857beb87dcb18581be3f1d1
george velimachitis <3185416+gvelim@users.noreply.github.com>
2024-12-06 13:42:09 +0000

    cosmetic (refactor later)

--- src/bin/day4/main.rs
+++ src/bin/day4/main.rs
@@ -2,26 +2,30 @@
 mod location;
 mod field;
 
+use std::time::Instant;
 use field::Field;
 use location::{Location,Direction};
 
 fn main() {
-    let input = std::fs::read_to_string("src/bin/day4/sample.txt").expect("File not found");
+    let input = std::fs::read_to_string("src/bin/day4/input.txt").expect("File not found");
     let field = input.parse::<Field<char>>().expect("Doesn't error");
     let (height, width) = (field.height(), field.width());
 
-    let sum = (0..width).map(|x| {
+    let t = Instant::now();
+    let sum = (0..width).map(|x|
         (0..height).map(|y|
             find_at_location(&field, "XMAS", Location(x,y), &[(1,0),(0,1),(1,1),(1,-1),(-1,0),(0,-1),(-1,-1),(-1,1)])
                 // .inspect(|r| println!("{:?}",r))
                 .count()
         )
         .sum::<usize>()
-    })
+    )
     .sum::<usize>();
-    println!("Part 1: Found ({sum}) XMAS words");
+    println!("Part 1: Found ({sum}) XMAS words - {:?}",t.elapsed());
+    assert_eq!(2603,sum);
 
-    let sum = (0..height).map(|y| {
+    let t = Instant::now();
+    let sum = (0..height).map(|y|
         (0..width)
             .filter(|&x|
                 (find_at_location(&field,"MAS",Location(x,y),&[(1,1)]).count() == 1 ||
@@ -31,39 +35,32 @@ fn main() {
             )
             // .inspect(|r| println!("Found at location: {:?}",(r,y)))
             .count()
-        })
+        )
         .sum::<usize>();
-
-    println!("Part 2: Found ({sum}) MAS crosses");
+    println!("Part 2: Found ({sum}) MAS crosses - {:?}",t.elapsed());
+    assert_eq!(1965,sum-1);
 }
 
 fn find_at_location(field: &Field<char>, word: &str, loc: Location, dirs: &[Direction]) -> impl Iterator<Item=(Location,Direction)> {
     dirs.iter()
         .copied()
-        .filter(move |&d|
-            find_in_direction(field, word, loc, d)
-        )
+        .filter(move |&d| find_in_direction(field, word, loc, d))
         .map(move |dir| (loc,dir))
 }
 
 fn find_in_direction(field: &Field<char>, word: &str, start: Location, dir: Direction) -> bool {
-    word
-        .char_indices()
-        // calculate location offset using current index and starting position
-        .map(|(i,c)|
-            start
-                .move_relative((dir.0 * i as isize, dir.1 * i as isize))
-                // .inspect(|p| print!("{:?},", (p.0,p.1,c)))
-                .map(|p| (p,c))
+    word.char_indices()
+        // calculate new location based on (a) current index (b) starting position & (c) direction
+        .map(|(i,c)| start
+            .move_relative((dir.0 * i as isize, dir.1 * i as isize))
+            .map(|p| (p,c))
         )
         // for offset location, check for matching letter
         .all(|val|
-            val.map(|(p,c)|
-                field
-                    .get_pos(p)
-                    // .inspect(|p| print!("={:?},", p))
-                    .map(|val| val == c)
-                    .unwrap_or(false)
+            val.map(|(p,c)| field
+                .get_pos(p)
+                .map(|val| val == c)
+                .unwrap_or(false)
             ).unwrap_or(false)
         )
 }
