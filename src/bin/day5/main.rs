c07815de8091d5d81536ee6eca16304b889a3942
gvelim <3185416+gvelim@users.noreply.github.com>
2024-12-07 13:11:16 +0000

    readability

--- src/bin/day5/main.rs
+++ src/bin/day5/main.rs
@@ -1,7 +1,7 @@
 mod update;
 mod order;
 
-use std::rc::Rc;
+use std::{rc::Rc, time::Instant};
 use order::OrderRules;
 use update::ManualUpdates;
 
@@ -17,21 +17,23 @@ fn main() {
         .map(|line| line.parse::<ManualUpdates>().unwrap())
         .collect::<Rc<[_]>>();
 
-    let validator = ManualUpdates::make_validator(&rules);
+    let is_valid_order = ManualUpdates::make_validator(&rules);
+    let t = Instant::now();
     let score = manual_updates
         .iter()
-        .filter(|&u| validator(u))
-        .map(|updates| updates.middle())
+        .filter(|&update| is_valid_order(update))
+        .map(|update| update.middle())
         .sum::<usize>();
-    println!("Part 1: valid updates score: {score}");
+    println!("Part 1: valid updates score: {score} - {:?}",t.elapsed());
     assert_eq!(6949,score);
 
+    let t = Instant::now();
     let score = manual_updates
         .iter()
-        .filter(|u| !validator(u))
+        .filter(|update| !is_valid_order(update))
         .map(ManualUpdates::sort_update(&rules))
-        .map(|u| u.middle())
+        .map(|update| update.middle())
         .sum::<usize>();
-    println!("Part 2: Score for fixed updates : {score}");
+    println!("Part 2: Score for fixed updates : {score} - {:?}",t.elapsed());
     assert_eq!(4145,score);
 }
