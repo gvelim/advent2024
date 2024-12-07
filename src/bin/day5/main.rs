60b290df13d0a7c182aca1dda2d64ceae964dcfe
gvelim <3185416+gvelim@users.noreply.github.com>
2024-12-07 12:41:44 +0000

    part 2 pass

--- src/bin/day5/main.rs
+++ src/bin/day5/main.rs
@@ -1,50 +1,85 @@
 mod update;
 mod order;
-mod entry;
 
+use std::rc::Rc;
 use order::OrderRules;
 use update::ManualUpdates;
 
 fn main() {
     let input = std::fs::read_to_string("src/bin/day5/input.txt").expect("msg");
     let mut s = input.split("\n\n");
-    let rules_str = s.next().unwrap();
+
+    let rules = s.next().unwrap()
+        .parse::<OrderRules>()
+        .unwrap();
     let updates = s.next().unwrap()
         .lines()
-        .map(|line| line.parse::<ManualUpdates>().expect("msg"));
+        .map(|line| line.parse::<ManualUpdates>().unwrap())
+        .collect::<Rc<[_]>>();
 
-    let rules = rules_str.parse::<OrderRules>().expect("msg");
     let validator = make_validator(&rules);
-
-    let pass = updates
+    let score = updates
+        .iter()
         // .inspect(|d| print!("{:?}",d))
-        .filter(validator)
+        .filter(|&u| validator(u))
         .map(|updates| updates.middle())
         // .inspect(|s| println!("{s}"))
         .sum::<usize>();
-    println!("Part 1: valid updates score: {pass}");
+    println!("Part 1: valid updates score: {score}");
+    assert_eq!(6949,score);
+
+    let score = updates
+        .iter()
+        .filter(|u| !validator(u))
+        .map(sort_update(&rules))
+        .map(|u| u.middle())
+        .sum::<usize>();
+    println!("Part 2: Score for fixed updates : {score}");
+    assert_eq!(4145,score);
+
+
 }
 
+fn sort_update(rules: &OrderRules) ->  impl Fn(&ManualUpdates) -> ManualUpdates {
+    use std::cmp;
+
+    |updates: &ManualUpdates| {
+
+        let mut list = updates.entries().cloned().collect::<Vec<_>>();
+
+        list.sort_by(|&a,b| {
+            rules
+                .pages_to_follow(a)
+                .map(|set|
+                    if set.contains(b) { cmp::Ordering::Less } else { cmp::Ordering::Greater }
+                )
+                .unwrap_or(cmp::Ordering::Equal)
+        });
+
+        ManualUpdates { list }
+    }
+}
 
 fn make_validator(rules: &OrderRules) ->  impl Fn(&ManualUpdates) -> bool {
     |updates: &ManualUpdates| {
+        // println!(">> {updates:?}");
         updates
             .entries()
+            .enumerate()
             // is each page followed by the correct pages ?
-            .all(|entry|{
-                rules
-                    .pages_to_follow(entry.page)
-                    // .inspect(|p| print!("{:?} => {:?}, ",entry,p))
-                    .map(|pages| {
+            .all(|(i, &page)|{
+                rules.pages_to_follow(page)
+                    // .inspect(|p| print!("{:?} => {:?}, ",page,p))
+                    .map(|pages | {
                         // pages that MUST follow in the update
-                        pages
-                            .iter()
+                        pages.iter()
                             // current page position < following page(s) positions
                             .all(|&follow_page|
                                 // page in the update list ?
                                 updates.contains(follow_page)
-                                    // .inspect(|p| print!("{:?},",p))
-                                    .map(|following| following > entry)
+                                    // .inspect(|p| print!("{:?}",(follow_page,p)))
+                                    .map(|following| following > i)
+                                    // .inspect(|p| print!("{},",if *p {"✓"} else {"✘"}))
                                     .unwrap_or(true)
                             )
                     })
--- src/bin/day5/update.rs
+++ src/bin/day5/update.rs
@@ -1,23 +1,21 @@
-use std::{collections::HashMap, fmt::Debug, num::ParseIntError, str::FromStr};
+use std::{fmt::Debug, num::ParseIntError, str::FromStr};
 use crate::order::Page;
-use super::entry::PrintEntry;
 
 pub(crate) struct ManualUpdates {
     // store (Key: Number, Val: {Page,Index})
     // we'll then use the rules to validate Xi < Yi
-    list: HashMap<Page,PrintEntry>,
-    middle: Page
+    pub list: Vec<Page>,
 }
 
 impl ManualUpdates {
-    pub fn entries(&self) -> impl Iterator<Item = &PrintEntry>  {
-        self.list.values()
+    pub fn entries(&self) -> impl Iterator<Item = &Page>  {
+        self.list.iter()
     }
-    pub fn contains(&self, p: Page) -> Option<&PrintEntry> {
-        self.list.get(&p)
+    pub fn contains(&self, p: Page) -> Option<usize> {
+        self.list.iter().position(|&e|p == e)
     }
     pub(crate) fn middle(&self) -> Page {
-        self.middle
+        self.list.get(self.list.len()/2).map_or(0_usize, |entry| *entry)
     }
 }
 
@@ -27,29 +25,20 @@ impl FromStr for ManualUpdates {
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         let list = s
             .split(',')
-            .enumerate()
-            .map(|(pos,numeric)|
-                numeric.parse::<usize>().map(|page|
-                    (page, PrintEntry{page,pos})
-                )
+            .map(|numeric|
+                numeric.parse::<usize>()
             )
             .collect::<Result<Vec<_>,_>>()?;
 
-        let middle = list.get( list.len()/2 )
-            .map_or(0_usize, |entry| entry.0);
-
-        Ok( ManualUpdates {
-            list: HashMap::from_iter(list),
-            middle
-        })
+        Ok( ManualUpdates {list})
     }
 }
 
 impl Debug for ManualUpdates {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         f.write_str("Updates")?;
-        f.debug_map().entries(
-            self.entries().map(|d| (d.page, d.pos))
+        f.debug_list().entries(
+            self.entries()
         ).finish()?;
         Ok(())
     }
