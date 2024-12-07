0b4472ba8492b54ac8fddd0f1c2ef6d7aca765bd
gvelim <3185416+gvelim@users.noreply.github.com>
2024-12-07 12:58:06 +0000

    rearrange code

--- src/bin/day5/main.rs
+++ src/bin/day5/main.rs
@@ -17,13 +17,11 @@ fn main() {
         .map(|line| line.parse::<ManualUpdates>().unwrap())
         .collect::<Rc<[_]>>();
 
-    let validator = make_validator(&rules);
+    let validator = ManualUpdates::make_validator(&rules);
     let score = manual_updates
         .iter()
-        // .inspect(|d| print!("{:?}",d))
         .filter(|&u| validator(u))
         .map(|updates| updates.middle())
-        // .inspect(|s| println!("{s}"))
         .sum::<usize>();
     println!("Part 1: valid updates score: {score}");
     assert_eq!(6949,score);
@@ -31,41 +29,9 @@ fn main() {
     let score = manual_updates
         .iter()
         .filter(|u| !validator(u))
-        .map(sort_update(&rules))
+        .map(ManualUpdates::sort_update(&rules))
         .map(|u| u.middle())
         .sum::<usize>();
     println!("Part 2: Score for fixed updates : {score}");
     assert_eq!(4145,score);
-
-
-}
-
-fn make_validator(rules: &OrderRules) ->  impl Fn(&ManualUpdates) -> bool {
-    |updates: &ManualUpdates| {
-        let tmp = sort_update(rules)(updates);
-
-        tmp.entries()
-            .zip(updates.entries())
-            .all(|(a,b)| a == b)
-    }
-}
-
-fn sort_update(rules: &OrderRules) ->  impl Fn(&ManualUpdates) -> ManualUpdates {
-    use std::cmp;
-
-    |updates: &ManualUpdates| {
-
-        let mut list = updates.entries().cloned().collect::<Vec<_>>();
-
-        list.sort_by(|&a,b| {
-            rules
-                .pages_to_follow(a)
-                .map(|set|
-                    if set.contains(b) { cmp::Ordering::Less } else { cmp::Ordering::Greater }
-                )
-                .unwrap_or(cmp::Ordering::Equal)
-        });
-
-        ManualUpdates { list }
-    }
 }
--- src/bin/day5/update.rs
+++ src/bin/day5/update.rs
@@ -1,19 +1,42 @@
 use std::{fmt::Debug, num::ParseIntError, str::FromStr};
-use crate::order::Page;
+use super::order::Page;
+use super::OrderRules;
 
 pub(crate) struct ManualUpdates {
-    // store (Key: Number, Val: {Page,Index})
-    // we'll then use the rules to validate Xi < Yi
-    pub list: Vec<Page>,
+    list: Vec<Page>,
 }
 
 impl ManualUpdates {
+    pub fn make_validator(rules: &OrderRules) ->  impl Fn(&ManualUpdates) -> bool {
+        |updates: &ManualUpdates| {
+            let tmp = Self::sort_update(rules)(updates);
+            tmp.entries()
+                .zip(updates.entries())
+                .all(|(a,b)| a == b)
+        }
+    }
+
+    pub fn sort_update(rules: &OrderRules) ->  impl Fn(&ManualUpdates) -> ManualUpdates {
+        use std::cmp;
+
+        |updates: &ManualUpdates| {
+            let mut list = updates.entries().cloned().collect::<Vec<_>>();
+            list.sort_by(|&a,b| {
+                rules
+                    .pages_to_follow(a)
+                    .map(|set|
+                        if set.contains(b) { cmp::Ordering::Less } else { cmp::Ordering::Greater }
+                    )
+                    .unwrap_or(cmp::Ordering::Equal)
+            });
+            ManualUpdates { list }
+        }
+    }
+
     pub fn entries(&self) -> impl Iterator<Item = &Page>  {
         self.list.iter()
     }
-    pub fn contains(&self, p: Page) -> Option<usize> {
-        self.list.iter().position(|&e|p == e)
-    }
+
     pub(crate) fn middle(&self) -> Page {
         self.list.get(self.list.len()/2).map_or(0_usize, |entry| *entry)
     }
@@ -23,23 +46,18 @@ impl FromStr for ManualUpdates {
     type Err = ParseIntError;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let list = s
-            .split(',')
-            .map(|numeric|
-                numeric.parse::<usize>()
-            )
-            .collect::<Result<Vec<_>,_>>()?;
-
-        Ok( ManualUpdates {list})
+        Ok( ManualUpdates {
+            list : s.split(',')
+                .map(|numeric| numeric.parse::<usize>())
+                .collect::<Result<Vec<_>,_>>()?
+        })
     }
 }
 
 impl Debug for ManualUpdates {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        f.write_str("Updates")?;
-        f.debug_list().entries(
-            self.entries()
-        ).finish()?;
+        f.write_str("Updates:")?;
+        f.debug_list().entries(self.entries()).finish()?;
         Ok(())
     }
 }
