0ac808de7c5058c802cf10f0652cd856446a5740
gvelim <3185416+gvelim@users.noreply.github.com>
2024-12-15 00:46:15 +0000

    Refactor logic

--- src/bin/day8/main.rs
+++ src/bin/day8/main.rs
@@ -1,27 +1,29 @@
 use std::{collections::HashMap, ops::RangeInclusive, str::FromStr, time::Instant};
 use advent2024::{field::Field, location::Location};
-use itertools::*;
+use itertools::Itertools;
 
 fn main() {
-    let input = std::fs::read_to_string("src/bin/day8/sample.txt").unwrap();
+    let input = std::fs::read_to_string("src/bin/day8/input.txt").unwrap();
     let city = input.parse::<City>().expect("Failed to parse City");
 
     let t = Instant::now();
     let count = city.antinodes(1..=1).unique().count();
     println!("Part 1: {:?} unique locations within the bounds of the map contain an antinode - {:?}",count, t.elapsed());
+    assert_eq!(247,count);
 
     let t = Instant::now();
-    let count = city.antinodes(0..=10).unique().count();
-    println!("Part 1: {:?} unique locations within the bounds of the map contain an antinode - {:?}",count, t.elapsed());
+    let count = city.antinodes(0..=100).unique().count();
+    println!("Part 2: {:?} unique locations contain an antinode given the effects of resonant harmonics - {:?}",count, t.elapsed());
+    assert_eq!(861,count);
 }
 
 #[derive(Debug,Clone, Copy)]
 struct  Antenna(Location);
 
 impl Antenna {
-    pub fn antinodes(&self, rhs: Antenna, resonance: usize) -> [Option<Location>;2] {
+    pub fn antinodes(&self, rhs: Antenna, harmonics: usize) -> [Option<Location>;2] {
         let (dxu,dyu) = self.0.distance(&rhs.0);
-        let (dx,dy) = ((resonance * dxu) as isize, (resonance * dyu) as isize);
+        let (dx,dy) = ((harmonics * dxu) as isize, (harmonics * dyu) as isize);
         match (self.0.0 >= rhs.0.0, self.0.1 >= rhs.0.1) {
             (true, true) => [rhs.0.move_relative((-dx,-dy)), self.0.move_relative((dx,dy))],
             (true, false) => [rhs.0.move_relative((-dx,dy)), self.0.move_relative((dx,-dy))],
@@ -37,32 +39,29 @@ struct City {
 }
 
 impl City {
-    pub fn antinodes(&self, resonance:RangeInclusive<usize>) -> impl Iterator<Item = Location> {
+    pub fn antinodes(&self, harmonics:RangeInclusive<usize>) -> impl Iterator<Item = Location> {
         self.antennas
-            .iter()
-            .flat_map(move |(_, a)|
-                a.iter()
-                    .tuple_combinations()
-                    .flat_map({
-                        let res = resonance.clone();
-                        move |(a,&b)|
-                        res.clone()
-                            .take_while(move |&r|{
-                                let res = a.antinodes(b,r);
-                                match (res[0],res[1]) {
-                                    (None, None) => false,
-                                    (None, Some(l)) if self.city.get(l).is_some() => true,
-                                    (Some(l), None) if self.city.get(l).is_some() => true,
-                                    (Some(a), Some(b)) if self.city.get(a).is_some() || self.city.get(b).is_some() => true,
-                                    _ => false
-                                }
-                            })
-                            .map(move |r| a.antinodes(b,r))
-                    })
+            .values()
+            .flat_map(move |antennas| antennas
+                .iter()
+                .tuple_combinations()
+                .flat_map({
+                    let res = harmonics.clone();
+                    move |(a, b)| res
+                        .clone()
+                        .map(|harmonic| a.antinodes(*b, harmonic))
+                        .take_while(|&antinodes| {
+                            match (antinodes[0], antinodes[1]) {
+                                (_, Some(l)) if self.city.get(l).is_some() => true,
+                                (Some(l), _) if self.city.get(l).is_some() => true,
+                                _ => false
+                            }
+                        })
+                })
             )
-            .flat_map(|an| an.into_iter())
-            .filter_map(|loc|
-                loc.filter(|&loc| self.city.get(loc).is_some())
+            .flat_map(|antinodes| antinodes.into_iter())
+            .filter_map(|location|
+                location.filter(|&location| self.city.get(location).is_some())
             )
     }
 }
