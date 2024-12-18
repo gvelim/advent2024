5e1f9a80579f52c70acc31dd489fad8d9d9df7da
george velimachitis <3185416+gvelim@users.noreply.github.com>
2024-12-18 19:45:29 +0000

    day9 part 2 test pass

--- src/bin/day9/main.rs
+++ src/bin/day9/main.rs
@@ -13,11 +13,12 @@ fn main() {
     let fs = FileSystem::read_diskmap(&diskmap).collect::<Vec<_>>();
     let comp = FileSystem::compress(&fs).collect::<Vec<_>>();
     let chksum = FileSystem::checksum(&comp);
-    println!("Part 1: Checksum {:?}",chksum);
+    println!("Part 1: Checksum {:?} - {:?}",chksum,FileSystem::to_string(&comp));
     // assert_eq!(6225730762521,chksum);
-    FileSystem::move_files(&mut diskmap);
-    // let chksum = FileSystem::checksum(&comp);
-    println!("Part 2: Checksum {:?}, {:?}",0, diskmap);
+    let dfg_dm = FileSystem::move_files(&mut diskmap);
+    let dfg_fs = FileSystem::read_diskmap(dfg_dm).collect::<Vec<_>>();
+    let chksum = FileSystem::checksum(&dfg_fs);
+    println!("Part 2: Checksum {:?} - {:?}",chksum,FileSystem::to_string(&dfg_fs));
 }
 
 #[derive(Debug)]
@@ -25,12 +26,11 @@ struct FileSystem;
 
 impl FileSystem {
     fn read_diskmap(map: &DiskMap) -> impl Iterator<Item=(isize,u8)> {
-        let mut id_gen = sequence(0);
         map.iter()
             .enumerate()
             .flat_map(move |(i, &c)| {
                 repeat(
-                    if i % 2 == 0 { (id_gen(1),c.0 as u8) } else { (-1,c.0 as u8) }
+                    if i % 2 == 0 { (c.1,c.0 as u8) } else { (-1,c.0 as u8) }
                 ).take(c.0 as usize)
             })
     }
@@ -51,25 +51,30 @@ impl FileSystem {
             .map(|(i, c)| i * (c.0 as usize))
             .sum::<usize>()
     }
-    fn move_files(dm: &mut DiskMap) {
+    fn move_files(dm: &mut DiskMap) -> &DiskMap {
         let files = dm.files().collect::<Vec<_>>();
-        let mut offset = 0;
 
         for file in files.iter().rev() {
-            let f_pos = (file.1 * 2) as usize + offset;
+            let f_pos = dm.iter().position(|e| e == file).unwrap();
             print!("File to move: {:?}", (file,f_pos));
-            // find space
             let space = dm.iter().filter(|e| e.1.eq(&-1)).find_position(|space| space.0 >= file.0);
             if let Some((pos, space)) = space  {
                 let s_pos = pos * 2 + 1;
-                if space.0 == 0 || s_pos >= f_pos { continue; }
+                if space.0 == 0 || s_pos >= f_pos { println!(" - miss"); continue; }
                 print!(" into: {:?}", (s_pos, space));
                 dm.remove_file(f_pos).insert_file(s_pos, *file);
                 print!(", remove at {} & insert at {}", f_pos, s_pos);
-                offset += 2;
             }
             println!("\n\t{:?}",dm);
         }
+        dm
+    }
+    fn to_string(fs: &[(isize, u8)]) -> String {
+        fs.iter()
+            .flat_map(|&(i,c)|{
+                repeat(if i == -1 {'.'} else { (i as u8 + b'0') as char }).take(1)
+            })
+            .collect::<String>()
     }
 }
 
