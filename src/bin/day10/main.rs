eda2b203f6ffb8aa6be950a3bbd31a4211c68a20
gvelim <200~3185416+gvelim@users.noreply.github.com>
2024-12-22 10:29:59 +0000

    Part 1 WIP

--- src/bin/day10/main.rs
+++ src/bin/day10/main.rs
@@ -1,3 +1,22 @@
+use std::rc::Rc;
+
+use advent2024::{location::*, field::Field};
+
 fn main() {
     todo!()
+}
+
+type TopographicalMap = Field<u8>;
+
+struct Trail {
+    map: Rc<TopographicalMap>,
+    pos: Location
+}
+
+impl Iterator for Trail {
+    type Item = u8;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        todo!()
+    }
 }
