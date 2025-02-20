501145d4f9520f952820772d9956cec693670157
gvelim <3185416+gvelim@users.noreply.github.com>
2025-02-20 08:05:55 +0000

    parser unit test

--- src/bin/day12/main.rs
+++ src/bin/day12/main.rs
@@ -1,6 +1,7 @@
 mod segment;
 
 use std::collections::{BTreeSet, HashMap};
+use advent2024::id_generator;
 use segment::{extract_ranges, PlotSegment};
 
 fn main() {
@@ -17,13 +18,13 @@ fn main() {
 // parser extracts and composes plots per scanline
 // a plot is composed out of multiple scanlines
 fn parse_garden(input: &str) -> HashMap<usize, BTreeSet<(usize,PlotSegment)>> {
-    // parsing logic is as follows:
     // set active map structures 1 & 2; holding (K,V) as (active segment, ID)
     let mut actseg1: Vec<(PlotSegment, usize, bool)> = Vec::new();
     let mut actseg2: Vec<(PlotSegment, usize, bool)> = Vec::new();
 
     // id generator fn()
-    let mut get_id = (|mut start: usize| move || { start += 1; start })(0);
+    let mut get_id = id_generator(0);
+
     // line counter
     let mut line = 0;
 
@@ -31,11 +32,11 @@ fn parse_garden(input: &str) -> HashMap<usize, BTreeSet<(usize,PlotSegment)>> {
         .map(extract_ranges)
         .enumerate()
         // for each line of plant segments(plant type, range)
-        .fold(HashMap::new(), |mut garden, (l, mut segments)| {
+        .fold(HashMap::<usize,BTreeSet<(usize,PlotSegment)>>::new(), |mut garden, (l, segments)| {
             line = l;
 
             // for each plant segment
-            while let Some(segment) = segments.next() {
+            for segment in segments {
 
                 // find within map 1, all active segments indeces that (a) overlap with && (b) have same plant type and flag those as matched
                 let mut matched = actseg1
@@ -54,6 +55,7 @@ fn parse_garden(input: &str) -> HashMap<usize, BTreeSet<(usize,PlotSegment)>> {
                 // if empty, then push a new (K,V) (segment, ID) into active segments map 2 and process next segment
                 if matched.is_empty() {
                     actseg2.push((segment, get_id() , false));
+                    continue;
                 } else {
                     // push new segment to active segments map 2 using same ID
                     let id = actseg1[ matched[0] ].1;
@@ -61,7 +63,7 @@ fn parse_garden(input: &str) -> HashMap<usize, BTreeSet<(usize,PlotSegment)>> {
                     // pop active segment(s) and push into garden map using same ID and current line number
                     while let Some(index) = matched.pop() {
                         garden.entry(id)
-                            .or_insert(BTreeSet::new())
+                            .or_default()
                             .insert((line, actseg1[index].0.clone()));
                     }
                 }
@@ -70,7 +72,7 @@ fn parse_garden(input: &str) -> HashMap<usize, BTreeSet<(usize,PlotSegment)>> {
             // Empty map 1 and move any unmatched active segments to the garden map using same ID and current line number
             while let Some((seg, id, m)) = actseg1.pop() {
                 if !m {
-                    garden.entry(id).or_insert(BTreeSet::new()).insert((line, seg));
+                    garden.entry(id).or_default().insert((line, seg));
                 }
             }
 
@@ -81,7 +83,7 @@ fn parse_garden(input: &str) -> HashMap<usize, BTreeSet<(usize,PlotSegment)>> {
 
     // Move any leftover active segments to the garden map
     while let Some((seg, id, _)) = actseg1.pop() {
-        garden.entry(id).or_insert(BTreeSet::new()).insert((line+1, seg));
+        garden.entry(id).or_default().insert((line+1, seg));
     }
 
     // return garden map
@@ -94,5 +96,36 @@ fn area(rows: &BTreeSet<(usize,PlotSegment)>) -> usize {
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+
+    #[test]
+    fn test_garden_parser() {
+        let input = std::fs::read_to_string("src/bin/day12/sample.txt").unwrap();
+        let garden = parse_garden(&input);
+
+        // A region of R plants with price 12 * 18 = 216.
+        assert_eq!(area(&garden[&1]), 12, "expected 12 got {:?}",garden[&1]);
+        // A region of I plants with price 4 * 8 = 32.
+        assert_eq!(area(&garden[&2]), 4, "expected 4 got {:?}",garden[&2]);
+        // A region of C plants with price 14 * 28 = 392.
+        assert_eq!(area(&garden[&3]), 14, "expected 14 got {:?}",garden[&3]);
+        // A region of F plants with price 10 * 18 = 180.
+        assert_eq!(area(&garden[&4]), 10, "expected 10 got {:?}",garden[&4]);
+        // A region of V plants with price 13 * 20 = 260.
+        assert_eq!(area(&garden[&5]), 13, "expected 13 got {:?}",garden[&5]);
+        // A region of J plants with price 11 * 20 = 220.
+        assert_eq!(area(&garden[&6]), 11, "expected 11 got {:?}",garden[&6]);
+        // A region of C plants with price 1 * 4 = 4.
+        assert_eq!(area(&garden[&7]), 1, "expected 1 got {:?}",garden[&7]);
+        // A region of E plants with price 13 * 18 = 234.
+        assert_eq!(area(&garden[&8]), 13, "expected 13 got {:?}",garden[&8]);
+        // A region of I plants with price 14 * 22 = 308.
+        assert_eq!(area(&garden[&9]), 14, "expected 14 got {:?}",garden[&9]);
+        // A region of M plants with price 5 * 12 = 60.
+        assert_eq!(area(&garden[&10]), 5, "expected 5 got {:?}",garden[&10]);
+        // A region of S plants with price 3 * 8 = 24.
+        assert_eq!(area(&garden[&11]), 3, "expected 3 got {:?}",garden[&11]);
+
+    }
 
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,7 @@
 pub mod field;
 pub mod location;
+
+// id generator fn()
+pub fn id_generator(mut start: usize) -> impl FnMut() -> usize {
+  move || { start += 1; start }
+}
