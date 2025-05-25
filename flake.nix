c62c83ca7eab24e5026428fd915b63569a69ced3
gvelim <3185416+gvelim@users.noreply.github.com>
2025-05-25 18:47:38 +0100

    Refactor dev shell configuration
    Extract common configuration into a reusable function.

--- flake.nix
+++ flake.nix
@@ -5,25 +5,31 @@
         nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     };
 
-    outputs = {self, nixpkgs} : {
+    outputs = {self, nixpkgs} :
+    let
+        # creating a function that given a pkgs input
+        # builds the attribute set for input the mkShell()
+        dev_shell = pkgs: {
+            packages = with pkgs; [
+                rustc
+                cargo
+                nil
+                nixd
+                git
+            ];
+            shellHook = ''
+                echo "Welcome to the Advent2024 development environment!"
+                /Applications/Zed.app/Contents/MacOS/zed . &
+            '';
+        };
+    in
+    {
         devShells.aarch64-darwin =
             let
                 pkgs = nixpkgs.legacyPackages.aarch64-darwin;
             in
             {
-                default = pkgs.mkShell {
-                    packages = [
-                        pkgs.rustc
-                        pkgs.cargo
-                        pkgs.nil
-                        pkgs.nixd
-                        pkgs.git
-                    ];
-                shellHook = ''
-                    echo "Welcome to the Advent2024 development environment!"
-                    /Applications/Zed.app/Contents/MacOS/zed . &
-                '';
-                };
+                default = pkgs.mkShell dev_shell pkgs;
             };
     };
 }
