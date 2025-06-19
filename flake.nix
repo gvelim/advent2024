f1865dcb6835310b97f1152259fbd7ea02f7cc0c
gvelim <3185416+gvelim@users.noreply.github.com>
2025-06-19 19:36:33 +0100

    Use system instead of target in shell setup
    The `mkShell_attrSet` function was updated to use the `system` parameter instead
    of `target` when constructing the path to the Rust toolchain binaries. This
    aligns with how the system is defined in the Nix configuration.

--- flake.nix
+++ flake.nix
@@ -10,50 +10,49 @@
 
     outputs = {self, nixpkgs} :
     let
-    overrides = builtins.fromTOML (builtins.readFile (self + "/rust-toolchain.toml"));
-    # Function that takes a Nixpkgs package set and returns an attribute set
-    # suitable for passing to pkgs.mkShell. The attribute set includes:
-    # - packages: List of packages to include in the shell environment
-    # - shellHook: Commands to run when entering the shell environment
-    mkShell_attrSet = {pkgs, target}: rec {
-        packages = with pkgs; [
-            rustup
-            nil
-            nixd
-            git
-        ];
-        RUSTC_VERSION = overrides.toolchain.channel;
-        shellHook = ''
-            export PATH=$PATH:''${CARGO_HOME:-~/.cargo}/bin
-            # Dynamically determine the Rust system string (architecture-os) for the current system
-            export PATH=$PATH:''${RUSTUP_HOME:-~/.rustup}/toolchains/${RUSTC_VERSION}-${target}/bin/
-            echo "Welcome to the Advent2024 development environment!"
-            # /Applications/Zed.app/Contents/MacOS/zed . &
-        '';
+        overrides = builtins.fromTOML (builtins.readFile (self + "/rust-toolchain.toml"));
+        # Function that takes a Nixpkgs package set and returns an attribute set
+        # suitable for passing to pkgs.mkShell. The attribute set includes:
+        # - packages: List of packages to include in the shell environment
+        # - shellHook: Commands to run when entering the shell environment
+        mkShell_attrSet = pkgs: system: rec {
+            packages = with pkgs; [
+                rustup
+                nil
+                nixd
+                git
+            ];
+            RUSTC_VERSION = overrides.toolchain.channel;
+            shellHook = ''
+                export PATH=$PATH:''${CARGO_HOME:-~/.cargo}/bin
+                # Dynamically determine the Rust system string (architecture-os) for the current system
+                export PATH=$PATH:''${RUSTUP_HOME:-~/.rustup}/toolchains/${RUSTC_VERSION}-${system}/bin/
+                echo "Welcome to the Advent2024 development environment!"
+                # /Applications/Zed.app/Contents/MacOS/zed . &
+            '';
 
-    };
-    # This function takes a platform (like "aarch64-darwin") as input and:
-    # 1. Gets the nixpkgs package set for that platform
-    # 2. Creates a development shell using mkShell with the packages
-    #    and shellHook defined in dev_shell
-    # 3. Returns it as the default devShell for that platform
-    build_DevShell = platform :
-        let
-            pkgs = nixpkgs.legacyPackages.${platform};
-        in
-        {
-            default = pkgs.mkShell (
-                mkShell_attrSet {
-                    pkgs = pkgs;
-                    target = pkgs.stdenv.hostPlatform.config;
-                }
-            );
         };
-    platforms = [
-        "aarch64-darwin"
-        "x86_64-darwin"
-        "x86_64-linux"
-    ];
+        # This function takes a platform (like "aarch64-darwin") as input and:
+        # 1. Gets the nixpkgs package set for that platform
+        # 2. Creates a development shell using mkShell with the packages
+        #    and shellHook defined in dev_shell
+        # 3. Returns it as the default devShell for that platform
+        build_DevShell = system :
+            let
+                pkgs = import nixpkgs { inherit system; };
+                # equivalent to
+                # pkgs = nixpkgs.legacyPackages.${system};
+            in
+            {
+                default = pkgs.mkShell (
+                    mkShell_attrSet pkgs system
+                );
+            };
+        platforms = [
+            "aarch64-darwin"
+            "x86_64-darwin"
+            "x86_64-linux"
+        ];
     in
     {
         # Generate development shells for specified platforms.
@@ -67,10 +66,10 @@
         #   aarch64-darwin = { default = <aarch64-darwin dev shell>; };
         #   x86_64-darwin = { default = <x86_64-darwin dev shell>; };
         # }
-        devShells = nixpkgs.lib.genAttrs platforms  build_DevShell;
-        packages = nixpkgs.lib.genAttrs platforms (platform :
+        devShells = nixpkgs.lib.genAttrs platforms build_DevShell;
+        packages = nixpkgs.lib.genAttrs platforms (system :
             let
-              pkgs = nixpkgs.legacyPackages.${platform};
+              pkgs = import nixpkgs { inherit system; };
             in
             {
               # Define your Rust application package here
